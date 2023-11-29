35efc1de8e15b938e264211c75d74d07725e183d
s.tootle <sdtootle@gmail.com>
2023-11-29 13:34:17 -0800

    Test using unified memory and prefect

--- project/wavetoy_gpu/numerical_grids_and_timestep.cu
+++ project/wavetoy_gpu/numerical_grids_and_timestep.cu
@@ -24,10 +24,10 @@ void initialize_grid_gpu(params_struct *restrict params,
     xx1[j] = xxmin1 + ((REAL)(j - NGHOSTS) + (1.0 / 2.0)) * params->dxx1;
   for (int j = index; j < params->Nxx_plus_2NGHOSTS2; j+=stride)
     xx2[j] = xxmin2 + ((REAL)(j - NGHOSTS) + (1.0 / 2.0)) * params->dxx2;
-  printf("%f \n", xx0[25]);
 }
 
 void numerical_grids_and_timestep(commondata_struct *restrict commondata, griddata_struct *restrict griddata, bool calling_for_first_time) {
+  // Finding dt is trivial for a cartesian grid so we do it in serial
   commondata->dt = 1e30;
   cudaDeviceSynchronize();
   for (int grid = 0; grid < commondata->NUMGRIDS; grid++) {
@@ -63,20 +63,34 @@ void numerical_grids_and_timestep(commondata_struct *restrict commondata, gridda
       commondata->nn_0 = 0;
       commondata->t_0 = 0.0;
       commondata->time = 0.0;
+      calling_for_first_time=false;
     }
     commondata->dt = MIN(commondata->dt, commondata->CFL_FACTOR * MIN(params->dxx0, MIN(params->dxx1, params->dxx2))); // CFL condition
-    // malloc(&griddata[grid].xx[0],sizeof(REAL)*(params->Nxx_plus_2NGHOSTS0));
-    // malloc(&griddata[grid].xx[1],sizeof(REAL)*(params->Nxx_plus_2NGHOSTS1));
-    // malloc(&griddata[grid].xx[2],sizeof(REAL)*(params->Nxx_plus_2NGHOSTS2));
-    griddata[grid].xx[0] = (REAL *)malloc(sizeof(REAL) * params->Nxx_plus_2NGHOSTS0);
+
+    // Allocate storage for the discrete grid and ensure it initially resides on the GPU
+    cudaMallocManaged(&griddata[grid].xx[0], sizeof(REAL) * params->Nxx_plus_2NGHOSTS0);
     cudaCheckErrors(griddata[grid].xx[0], "Malloc failed");
-    griddata[grid].xx[1] = (REAL *)malloc(sizeof(REAL) * params->Nxx_plus_2NGHOSTS1);
+    cudaMemPrefetchAsync(griddata[grid].xx[0], sizeof(REAL) * params->Nxx_plus_2NGHOSTS0, 0);
+    cudaCheckErrors(griddata[grid].xx[0], "prefetch failed");
+    
+    cudaMallocManaged(&griddata[grid].xx[1], sizeof(REAL) * params->Nxx_plus_2NGHOSTS1);
     cudaCheckErrors(griddata[grid].xx[1], "Malloc failed");
-    griddata[grid].xx[2] = (REAL *)malloc(sizeof(REAL) * params->Nxx_plus_2NGHOSTS2);
+    cudaMemPrefetchAsync(griddata[grid].xx[1], sizeof(REAL) * params->Nxx_plus_2NGHOSTS1, 0);
+    cudaCheckErrors(griddata[grid].xx[0], "prefetch failed");
+
+    cudaMallocManaged(&griddata[grid].xx[2], sizeof(REAL) * params->Nxx_plus_2NGHOSTS2);
     cudaCheckErrors(griddata[grid].xx[2], "Malloc failed");
-    cudaDeviceSynchronize();
-    initialize_grid_gpu<<<1,1>>>(params, griddata[grid].xx[0], griddata[grid].xx[1], griddata[grid].xx[2]);
+    cudaMemPrefetchAsync(griddata[grid].xx[2], sizeof(REAL) * params->Nxx_plus_2NGHOSTS2, 0);
+    cudaCheckErrors(griddata[grid].xx[0], "prefetch failed");
+    
+    // Initialize grid coordinates
+    dim3 block_threads(1024,1,1);
+    dim3 grid_blocks(params->Nxx_plus_2NGHOSTS0 + 1024 - 1);
+    initialize_grid_gpu<<<grid_blocks, block_threads>>>(params, 
+                                                        griddata[grid].xx[0], 
+                                                        griddata[grid].xx[1], 
+                                                        griddata[grid].xx[2]);
     cudaCheckErrors(initialize_grid_gpu, "initialize failed");
     cudaDeviceSynchronize();
-  }  
+  }
 }
