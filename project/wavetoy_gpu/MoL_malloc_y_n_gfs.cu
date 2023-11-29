4244396213f719e210ebee4ab054076905c33a24
s.tootle <sdtootle@gmail.com>
2023-11-29 13:49:04 -0800

    Move - GPU version

--- project/wavetoy_gpu/MoL_malloc_y_n_gfs.cu
+++ project/wavetoy_gpu/MoL_malloc_y_n_gfs.cu
@@ -1,5 +1,6 @@
 #include "BHaH_defines.h"
 #include "BHaH_function_prototypes.h"
+#include "BHaH_gpu_defines.h"
 /*
  * Method of Lines (MoL) for "RK4" method: Allocate memory for "y_n_gfs" gridfunctions
  * - y_n_gfs are used to store data for the vector of gridfunctions y_i at t_n, at the start of each MoL timestep
@@ -9,7 +10,11 @@ void MoL_malloc_y_n_gfs(const commondata_struct *restrict commondata, const para
                         MoL_gridfunctions_struct *restrict gridfuncs) {
 #include "set_CodeParameters.h"
   const int Nxx_plus_2NGHOSTS_tot = Nxx_plus_2NGHOSTS0 * Nxx_plus_2NGHOSTS1 * Nxx_plus_2NGHOSTS2;
-  gridfuncs->y_n_gfs = (REAL *restrict)malloc(sizeof(REAL) * NUM_EVOL_GFS * Nxx_plus_2NGHOSTS_tot);
+  // gridfuncs->y_n_gfs = (REAL *restrict)malloc(sizeof(REAL) * NUM_EVOL_GFS * Nxx_plus_2NGHOSTS_tot);
+  cudaMallocManaged(&gridfuncs->y_n_gfs, sizeof(REAL) * NUM_EVOL_GFS * Nxx_plus_2NGHOSTS_tot);
+  cudaCheckErrors(gridfuncs->y_n_gfs, "Malloc failed");
+  cudaMemPrefetchAsync(gridfuncs->y_n_gfs, sizeof(REAL) * NUM_EVOL_GFS * Nxx_plus_2NGHOSTS_tot);
+  cudaCheckErrors(gridfuncs->y_n_gfs, "prefetch failed");
 
   gridfuncs->diagnostic_output_gfs = gridfuncs->y_nplus1_running_total_gfs;
   gridfuncs->diagnostic_output_gfs2 = gridfuncs->k_odd_gfs;
