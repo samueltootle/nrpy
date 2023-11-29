c1b4da8c974806d4bb79e0e9ce4177b85ee12186
s.tootle <sdtootle@gmail.com>
2023-11-29 14:27:56 -0800

    Enable initial data

--- project/wavetoy_gpu/main.cu
+++ project/wavetoy_gpu/main.cu
@@ -58,7 +58,7 @@ int main(int argc, const char *argv[]) {
   }
 
   // // Step 3: Finalize initialization: set up initial data, etc.
-  // initial_data(commondata, griddata);
+  initial_data(commondata, griddata);
 
   // Step 4: Allocate storage for non-y_n gridfunctions, needed for the Runge-Kutta-like timestepping
   for (int grid = 0; grid < commondata->NUMGRIDS; grid++)
