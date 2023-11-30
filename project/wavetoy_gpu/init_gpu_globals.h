7ac2ad9359d4e0cd4d883520a8a79afd640067ed
s.tootle <sdtootle@gmail.com>
2023-11-29 17:23:30 -0800

    wip - store common and params in __constant__

--- project/wavetoy_gpu/init_gpu_globals.h
+++ project/wavetoy_gpu/init_gpu_globals.h
@@ -1,5 +1,6 @@
 #include "BHaH_gpu_defines.h"
-__constant__ REAL wavespeed;
+__constant__ REAL d_commondata;
+__constant__ REAL d_params;
 __constant__ REAL FDPart1_Rational_5_2; 
 __constant__ REAL FDPart1_Rational_1_12;
 __constant__ REAL FDPart1_Rational_4_3;
