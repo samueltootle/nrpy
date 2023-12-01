cebe673d7a33b90a0dcad662e6b86e6a8ebc97f8
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-12-01 10:13:41 -0800

    add RK substeps

--- project/wavetoy_gpu2/BHaH_gpu_function_prototypes.h
+++ project/wavetoy_gpu2/BHaH_gpu_function_prototypes.h
@@ -12,3 +12,82 @@ __device__
 void exact_solution_single_Cartesian_point_gpu(const commondata_struct *restrict commondata, const params_struct *restrict params,
     const REAL xCart0, const REAL xCart1, const REAL xCart2,  REAL *restrict exact_soln_UUGF, REAL *restrict exact_soln_VVGF
 );
+
+__global__
+void rk_substep1_gpu(commondata_struct *restrict commondata, 
+                params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs,
+                REAL const dt,
+                size_t const N);
+__host__
+void rk_substep1(params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs, REAL const dt) ;
+
+__global__
+void rk_substep2_gpu(commondata_struct *restrict commondata, 
+                params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs,
+                REAL const dt,
+                size_t const N);
+__host__
+void rk_substep2(params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs, REAL const dt) ;
+
+__global__
+void rk_substep3_gpu(commondata_struct *restrict commondata, 
+                params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs,
+                REAL const dt,
+                size_t const N);
+__host__
+void rk_substep3(params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs, REAL const dt) ;
+
+__global__
+void rk_substep4_gpu(commondata_struct *restrict commondata, 
+                params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs,
+                REAL const dt,
+                size_t const N);
+__host__
+void rk_substep4(params_struct *restrict params,
+                REAL *restrict y_n_gfs,
+                REAL *restrict y_nplus1_running_total_gfs,
+                REAL *restrict k_odd_gfs,
+                REAL *restrict k_even_gfs,
+                REAL *restrict auxevol_gfs, REAL const dt) ;
+
+__host__
+void set_fd_constants();
+__host__
+void set_param_constants(params_struct *restrict params);
+__host__
+void set_commondata_constants(commondata_struct *restrict commondata);
--- project/wavetoy_gpu2/MoL_step_forward_in_time.cu
+++ project/wavetoy_gpu2/MoL_step_forward_in_time.cu
@@ -1,6 +1,7 @@
 #include "BHaH_defines.h"
 #include "BHaH_function_prototypes.h"
 #include "BHaH_gpu_defines.h"
+#include "BHaH_gpu_function_prototypes.h"
 /*
  * Method of Lines (MoL) for "RK4" method: Step forward one full timestep.
  *
@@ -16,32 +17,29 @@ void MoL_step_forward_in_time(commondata_struct *restrict commondata, griddata_s
   for (int grid = 0; grid < commondata->NUMGRIDS; grid++) {
     commondata->time = time_start + 0.00000000000000000e+00 * commondata->dt;
     set_param_constants(griddata[grid].params);
-    rhs_eval(commondata, 
-             griddata[grid].params, 
-             griddata[grid].gridfuncs.y_n_gfs, 
-             griddata[grid].gridfuncs.k_odd_gfs);
+
     // Set gridfunction aliases from gridfuncs struct
     // y_n gridfunctions
-  //   REAL *restrict y_n_gfs = griddata[grid].gridfuncs.y_n_gfs;
-  //   // Temporary timelevel & AUXEVOL gridfunctions:
-  //   REAL *restrict y_nplus1_running_total_gfs = griddata[grid].gridfuncs.y_nplus1_running_total_gfs;
-  //   REAL *restrict k_odd_gfs = griddata[grid].gridfuncs.k_odd_gfs;
-  //   REAL *restrict k_even_gfs = griddata[grid].gridfuncs.k_even_gfs;
-  //   REAL *restrict auxevol_gfs = griddata[grid].gridfuncs.auxevol_gfs;
-  //   params_struct *restrict params = &griddata[grid].params;
-  //   REAL *restrict xx[3];
-  //   for (int ww = 0; ww < 3; ww++)
-  //     xx[ww] = griddata[grid].xx[ww];
-  //   const int Nxx_plus_2NGHOSTS0 = griddata[grid].params.Nxx_plus_2NGHOSTS0;
-  //   const int Nxx_plus_2NGHOSTS1 = griddata[grid].params.Nxx_plus_2NGHOSTS1;
-  //   const int Nxx_plus_2NGHOSTS2 = griddata[grid].params.Nxx_plus_2NGHOSTS2;
-  //   rhs_eval(commondata, params, y_n_gfs, k_odd_gfs);
-  //   LOOP_ALL_GFS_GPS(i) {
-  //     const REAL k_odd_gfsL = k_odd_gfs[i];
-  //     const REAL y_n_gfsL = y_n_gfs[i];
-  //     y_nplus1_running_total_gfs[i] = (1.0 / 6.0) * commondata->dt * k_odd_gfsL;
-  //     k_odd_gfs[i] = (1.0 / 2.0) * commondata->dt * k_odd_gfsL + y_n_gfsL;
-  //   }
+    __attribute__((unused)) REAL *restrict y_n_gfs = griddata[grid].gridfuncs.y_n_gfs;
+    // Temporary timelevel & AUXEVOL gridfunctions:
+    __attribute__((unused)) REAL *restrict y_nplus1_running_total_gfs = griddata[grid].gridfuncs.y_nplus1_running_total_gfs;
+    __attribute__((unused)) REAL *restrict k_odd_gfs = griddata[grid].gridfuncs.k_odd_gfs;
+    __attribute__((unused)) REAL *restrict k_even_gfs = griddata[grid].gridfuncs.k_even_gfs;
+    __attribute__((unused)) REAL *restrict auxevol_gfs = griddata[grid].gridfuncs.auxevol_gfs;
+    __attribute__((unused)) params_struct *restrict params = griddata[grid].params;
+    __attribute__((unused)) REAL *restrict xx[3];
+    for (int ww = 0; ww < 3; ww++) {
+      xx[ww] = griddata[grid].xx[ww];
+    }
+
+    rhs_eval(commondata, params, y_n_gfs, k_odd_gfs);
+    rk_substep1(params,
+            y_n_gfs,
+            y_nplus1_running_total_gfs,
+            k_odd_gfs,
+            k_even_gfs,
+            auxevol_gfs,dt);
+
   //   apply_bcs(commondata, params, k_odd_gfs);
   }
   // // -={ END k1 substep }=-
