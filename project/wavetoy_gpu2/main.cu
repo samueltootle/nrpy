6de6b45429c57247c9834c7b9e27d4d1e87ee519
s.tootle <sdtootle@gmail.com>
2023-12-01 14:49:43 -0800

    add gpu prototypes header

--- project/wavetoy_gpu2/main.cu
+++ project/wavetoy_gpu2/main.cu
@@ -1,6 +1,7 @@
 #include "BHaH_defines.h"
 #include "BHaH_function_prototypes.h"
 #include "BHaH_gpu_defines.h"
+#include "BHaH_gpu_function_prototypes.h"
 #include "init_gpu_globals.h"
 /*
  * -={ main() function }=-
