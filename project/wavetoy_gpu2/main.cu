036b6c69ab3c9392c98ace6170c64dec6f5baf15
s.tootle <sdtootle@gmail.com>
2023-12-01 14:59:05 -0800

    full code

--- project/wavetoy_gpu2/main.cu
+++ project/wavetoy_gpu2/main.cu
@@ -59,21 +59,21 @@ int main(int argc, const char *argv[]) {
   }
 
   // // Step 5: MAIN SIMULATION LOOP
-  // while (commondata.time < commondata.t_final) { // Main loop to progress forward in time.
-  //   // Step 5.a: Main loop, part 1: Output diagnostics
+  while (commondata.time < commondata.t_final) { // Main loop to progress forward in time.
+    // Step 5.a: Main loop, part 1: Output diagnostics
     diagnostics(&commondata, griddata);
 
-  //   // Step 5.b: Main loop, part 2 (pre_MoL_step_forward_in_time): Prepare to step forward in time
-  //   // (nothing here; specify by setting pre_MoL_step_forward_in_time string in register_CFunction_main_c().)
+    // Step 5.b: Main loop, part 2 (pre_MoL_step_forward_in_time): Prepare to step forward in time
+    // (nothing here; specify by setting pre_MoL_step_forward_in_time string in register_CFunction_main_c().)
 
-  //   // Step 5.c: Main loop, part 3: Step forward in time using Method of Lines with RK4 algorithm,
-  //   //           applying Quadratic extrapolation, manually defined boundary conditions.
+    // Step 5.c: Main loop, part 3: Step forward in time using Method of Lines with RK4 algorithm,
+    //           applying Quadratic extrapolation, manually defined boundary conditions.
     MoL_step_forward_in_time(&commondata, griddata);
 
-  //   // Step 5.d: Main loop, part 4 (post_MoL_step_forward_in_time): Finish up step in time
-  //   // (nothing here; specify by setting post_MoL_step_forward_in_time string in register_CFunction_main_c().)
+    // Step 5.d: Main loop, part 4 (post_MoL_step_forward_in_time): Finish up step in time
+    // (nothing here; specify by setting post_MoL_step_forward_in_time string in register_CFunction_main_c().)
 
-  // } // End main loop to progress forward in time.
+  } // End main loop to progress forward in time.
 
   // Step 5: Free all allocated memory
   for (int grid = 0; grid < commondata.NUMGRIDS; grid++) {
