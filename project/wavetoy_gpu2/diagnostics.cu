20201471e95ab063afa5e201c9afd26db5e8befd
s.tootle <sdtootle@gmail.com>
2023-11-30 17:19:46 -0800

    Add diagnostics

--- project/wavetoy_gpu2/diagnostics.cu
+++ project/wavetoy_gpu2/diagnostics.cu
@@ -1,24 +1,62 @@
 #include "BHaH_defines.h"
 #include "BHaH_function_prototypes.h"
+#include "BHaH_gpu_defines.h"
+#include "BHaH_gpu_function_prototypes.h"
 /*
  * Diagnostics.
  */
+__global__
+void diagnostics_gpu(const commondata_struct commondata, 
+                     const params_struct *restrict params, 
+                     const REAL *restrict xx0,
+                     const REAL *restrict xx1,
+                     const REAL *restrict xx2,
+                     const REAL *restrict y_n_gfs,
+                     REAL* exact_soln_at_center_UUGF, 
+                     REAL* exact_soln_at_center_VVGF,
+                     REAL* num_soln_at_center_UUGF, 
+                     REAL* num_soln_at_center_VVGF) {
+  int const & Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
+  int const & Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
+  int const & Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
+  
+  const int i0_center = params->Nxx_plus_2NGHOSTS0 / 2;
+  const int i1_center = params->Nxx_plus_2NGHOSTS1 / 2;
+  const int i2_center = params->Nxx_plus_2NGHOSTS2 / 2;
+  
+  const int center_of_grid_idx = IDX3(i0_center, i1_center, i2_center);
+
+  *num_soln_at_center_UUGF = y_n_gfs[IDX4pt(UUGF, center_of_grid_idx)];
+  *num_soln_at_center_VVGF = y_n_gfs[IDX4pt(VVGF, center_of_grid_idx)];
+
+  exact_solution_single_Cartesian_point_gpu(&commondata, 
+                                         params, 
+                                         xx0[i0_center], 
+                                         xx1[i1_center], 
+                                         xx2[i2_center], 
+                                         exact_soln_at_center_UUGF,
+                                         exact_soln_at_center_VVGF);
+}
+
 void diagnostics(commondata_struct *restrict commondata, griddata_struct *restrict griddata) {
 
   const REAL currtime = commondata->time, currdt = commondata->dt, outevery = commondata->diagnostics_output_every;
+
+  REAL* d_exact_soln_at_center_UUGF, *d_exact_soln_at_center_VVGF;
+  REAL* d_num_soln_at_center_UUGF, *d_num_soln_at_center_VVGF;
+  cudaMalloc(&d_exact_soln_at_center_UUGF, sizeof(REAL));
+  cudaMalloc(&d_exact_soln_at_center_VVGF, sizeof(REAL));
+  cudaMalloc(&d_num_soln_at_center_UUGF, sizeof(REAL));
+  cudaMalloc(&d_num_soln_at_center_VVGF, sizeof(REAL));
   // Explanation of the if() below:
   // Step 1: round(currtime / outevery) rounds to the nearest integer multiple of currtime.
   // Step 2: Multiplying by outevery yields the exact time we should output again, t_out.
   // Step 3: If fabs(t_out - currtime) < 0.5 * currdt, then currtime is as close to t_out as possible!
   if (fabs(round(currtime / outevery) * outevery - currtime) < 0.5 * currdt) {
     for (int grid = 0; grid < commondata->NUMGRIDS; grid++) {
-      // Unpack griddata struct:
-      const REAL *restrict y_n_gfs = griddata[grid].gridfuncs.y_n_gfs;
-      REAL *restrict xx[3];
-      for (int ww = 0; ww < 3; ww++)
-        xx[ww] = griddata[grid].xx[ww];
-      const params_struct *restrict params = &griddata[grid].params;
-#include "set_CodeParameters.h"
+      REAL const convergence_factor = commondata->convergence_factor;
+      int const nn = commondata->nn;
+      REAL time = commondata->time;
 
       // 0D output
       {
@@ -32,19 +70,25 @@ void diagnostics(commondata_struct *restrict commondata, griddata_struct *restri
         if (outfile == NULL) {
           fprintf(stderr, "Error: Cannot open file %s for writing.\n", filename);
           exit(1);
-        }
+        }        
 
-        const int i0_center = Nxx_plus_2NGHOSTS0 / 2;
-        const int i1_center = Nxx_plus_2NGHOSTS1 / 2;
-        const int i2_center = Nxx_plus_2NGHOSTS2 / 2;
-        const int center_of_grid_idx = IDX3(i0_center, i1_center, i2_center);
-
-        const REAL num_soln_at_center_UUGF = y_n_gfs[IDX4pt(UUGF, center_of_grid_idx)];
-        const REAL num_soln_at_center_VVGF = y_n_gfs[IDX4pt(VVGF, center_of_grid_idx)];
+        REAL num_soln_at_center_UUGF, num_soln_at_center_VVGF;
         REAL exact_soln_at_center_UUGF, exact_soln_at_center_VVGF;
-        exact_solution_single_Cartesian_point(commondata, params, xx[0][i0_center], xx[1][i1_center], xx[2][i2_center], &exact_soln_at_center_UUGF,
-                                              &exact_soln_at_center_VVGF);
-
+        diagnostics_gpu<<<1,1>>>(*commondata, 
+                     griddata[grid].params, 
+                     griddata[grid].xx[0],
+                     griddata[grid].xx[1],
+                     griddata[grid].xx[2],
+                     griddata[grid].gridfuncs.y_n_gfs,
+                     d_exact_soln_at_center_UUGF, 
+                     d_exact_soln_at_center_VVGF,
+                     d_num_soln_at_center_UUGF, 
+                     d_num_soln_at_center_VVGF);
+        cudaMemcpy(&num_soln_at_center_UUGF, d_num_soln_at_center_UUGF, sizeof(REAL), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&num_soln_at_center_VVGF, d_num_soln_at_center_VVGF, sizeof(REAL), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&exact_soln_at_center_UUGF, d_exact_soln_at_center_UUGF, sizeof(REAL), cudaMemcpyDeviceToHost);
+        cudaMemcpy(&exact_soln_at_center_VVGF, d_exact_soln_at_center_VVGF, sizeof(REAL), cudaMemcpyDeviceToHost);
+        
         fprintf(outfile, "%e %e %e %e %e\n", time, fabs(fabs(num_soln_at_center_UUGF - exact_soln_at_center_UUGF) / exact_soln_at_center_UUGF),
                 fabs(fabs(num_soln_at_center_VVGF - exact_soln_at_center_VVGF) / (1e-16 + exact_soln_at_center_VVGF)), num_soln_at_center_UUGF,
                 exact_soln_at_center_UUGF);
@@ -53,7 +97,7 @@ void diagnostics(commondata_struct *restrict commondata, griddata_struct *restri
       }
     }
   }
-  progress_indicator(commondata, griddata);
-  if (commondata->time + commondata->dt > commondata->t_final)
-    printf("\n");
+  // progress_indicator(commondata, griddata);
+  // if (commondata->time + commondata->dt > commondata->t_final)
+  //   printf("\n");
 }
