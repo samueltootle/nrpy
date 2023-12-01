544f43d0f78fc9d966b1f05adbd0bdf49239be36
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-12-01 09:58:15 -0800

    GPU version

--- project/wavetoy_gpu2/rhs_eval.cu
+++ project/wavetoy_gpu2/rhs_eval.cu
@@ -1,14 +1,44 @@
 #include "BHaH_defines.h"
+#include "BHaH_gpu_defines.h"
+#include "BHaH_gpu_function_prototypes.h"
 /*
  * Set RHSs for wave equation.
  */
-void rhs_eval(const commondata_struct *restrict commondata, const params_struct *restrict params, const REAL *restrict in_gfs,
+__global__
+void rhs_eval_gpu(const commondata_struct *restrict commondata, 
+              const params_struct *restrict params, 
+              const REAL *restrict in_gfs,
               REAL *restrict rhs_gfs) {
-#include "set_CodeParameters.h"
-#pragma omp parallel for
-  for (int i2 = NGHOSTS; i2 < NGHOSTS + Nxx2; i2++) {
-    for (int i1 = NGHOSTS; i1 < NGHOSTS + Nxx1; i1++) {
-      for (int i0 = NGHOSTS; i0 < NGHOSTS + Nxx0; i0++) {
+
+// #include "set_CodeParameters.h"
+  const REAL & invdxx0 = d_params.invdxx0;
+  const REAL & invdxx1 = d_params.invdxx1;
+  const REAL & invdxx2 = d_params.invdxx2;
+
+  const int & Nxx0 = d_params.Nxx0;
+  const int & Nxx1 = d_params.Nxx1;
+  const int & Nxx2 = d_params.Nxx2;
+
+  const int & Nxx_plus_2NGHOSTS0 = d_params.Nxx_plus_2NGHOSTS0;
+  const int & Nxx_plus_2NGHOSTS1 = d_params.Nxx_plus_2NGHOSTS1;
+  const int & Nxx_plus_2NGHOSTS2 = d_params.Nxx_plus_2NGHOSTS2;
+
+  const int tid0  = blockIdx.x * blockDim.x + threadIdx.x;
+  const int tid1  = blockIdx.y * blockDim.y + threadIdx.y;
+  const int tid2  = blockIdx.z * blockDim.z + threadIdx.z;
+  
+  const int stride0 = blockDim.x * gridDim.x;
+  const int stride1 = blockDim.y * gridDim.y;
+  const int stride2 = blockDim.x * gridDim.z;
+  // if(tid0 == 0 && tid1 == 0 && tid2 == 0) {
+  //   printf("%f %f %f %u %u %u %u %u %u", 
+  //     invdxx0,invdxx1,invdxx2,
+  //     Nxx0,Nxx2,Nxx2,
+  //     Nxx_plus_2NGHOSTS0,Nxx_plus_2NGHOSTS0,Nxx_plus_2NGHOSTS0);
+  // }
+  for (int i2 = tid2+NGHOSTS; i2 < NGHOSTS + Nxx2; i2+=stride2) {
+    for (int i1 = tid1+NGHOSTS; i1 < NGHOSTS + Nxx1; i1+=stride1) {
+      for (int i0 = tid0+NGHOSTS; i0 < NGHOSTS + Nxx0; i0+=stride0) {
         /*
          * NRPy+-Generated GF Access/FD Code, Step 1 of 2:
          * Read gridfunction(s) from main memory and compute FD stencils as needed.
@@ -27,9 +57,12 @@ void rhs_eval(const commondata_struct *restrict commondata, const params_struct 
         const REAL uu_i2p1 = in_gfs[IDX4(UUGF, i0, i1, i2 + 1)];
         const REAL uu_i2p2 = in_gfs[IDX4(UUGF, i0, i1, i2 + 2)];
         const REAL vv = in_gfs[IDX4(VVGF, i0, i1, i2)];
-        const REAL FDPart1_Rational_5_2 = 5.0 / 2.0;
-        const REAL FDPart1_Rational_1_12 = 1.0 / 12.0;
-        const REAL FDPart1_Rational_4_3 = 4.0 / 3.0;
+        
+        // moved to __constant__ space
+        // const REAL FDPart1_Rational_5_2 = 5.0 / 2.0;
+        // const REAL FDPart1_Rational_1_12 = 1.0 / 12.0;
+        // const REAL FDPart1_Rational_4_3 = 4.0 / 3.0;
+        
         const REAL FDPart1tmp0 = -FDPart1_Rational_5_2 * uu;
         const REAL uu_dDD00 =
             ((invdxx0) * (invdxx0)) * (FDPart1_Rational_1_12 * (-uu_i0m2 - uu_i0p2) + FDPart1_Rational_4_3 * (uu_i0m1 + uu_i0p1) + FDPart1tmp0);
@@ -49,4 +82,29 @@ void rhs_eval(const commondata_struct *restrict commondata, const params_struct 
       } // END LOOP: for (int i0 = NGHOSTS; i0 < NGHOSTS+Nxx0; i0++)
     }   // END LOOP: for (int i1 = NGHOSTS; i1 < NGHOSTS+Nxx1; i1++)
   }     // END LOOP: for (int i2 = NGHOSTS; i2 < NGHOSTS+Nxx2; i2++)
+}
+
+__host__
+void rhs_eval(const commondata_struct *restrict commondata, 
+              const params_struct *restrict params, 
+              const REAL *restrict in_gfs,
+              REAL *restrict rhs_gfs) {
+  
+  int Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2;
+  cudaMemcpy(&Nxx_plus_2NGHOSTS0, &params->Nxx_plus_2NGHOSTS0, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaCheckErrors(cudaMemcpy, "memory failed")
+  cudaMemcpy(&Nxx_plus_2NGHOSTS1, &params->Nxx_plus_2NGHOSTS1, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaCheckErrors(cudaMemcpy, "memory failed")
+  cudaMemcpy(&Nxx_plus_2NGHOSTS2, &params->Nxx_plus_2NGHOSTS2, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaCheckErrors(cudaMemcpy, "memory failed")
+
+  dim3 block(GPU_NBLOCK0,GPU_NBLOCK1,GPU_NBLOCK2);
+  dim3 grid(
+    (Nxx_plus_2NGHOSTS0 + GPU_NBLOCK0 - 1) / GPU_NBLOCK0,
+    (Nxx_plus_2NGHOSTS1 + GPU_NBLOCK1 - 1) / GPU_NBLOCK1,
+    (Nxx_plus_2NGHOSTS2 + GPU_NBLOCK2 - 1) / GPU_NBLOCK2
+  );
+  rhs_eval_gpu<<<grid,block>>>(commondata, params, in_gfs, rhs_gfs);
+  cudaCheckErrors(rhs_eval_gpu, "kernel failed")
+  // testcpy(in_gfs);
 }
