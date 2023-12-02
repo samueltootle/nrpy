d0638c7a680139ec74036caee80ea9f5016f4757
s.tootle <sdtootle@gmail.com>
2023-12-01 16:19:29 -0800

    New attempt at apply_bc - wrong results

--- project/wavetoy_gpu2/apply_bcs.cu
+++ project/wavetoy_gpu2/apply_bcs.cu
@@ -19,38 +19,35 @@ const int MINFACE = +1;
       }
 
 __global__
-void apply_bcs_gpu(const params_struct *restrict params, REAL *restrict gfs) {
+void update_face(int which_gf, const params_struct *restrict params, REAL *restrict gfs,
+                   int imin0, int imax0,
+                   int imin1, int imax1,
+                   int imin2, int imax2,
+                   const int FACEX0,
+                   const int FACEX1,
+                   const int FACEX2) {
   // Global data index - expecting a 1D dataset
   // Thread indices
-  const int tid = threadIdx.x + blockIdx.x*blockDim.x;
+  const int tid0  = blockIdx.x * blockDim.x + threadIdx.x;
+  const int tid1  = blockIdx.y * blockDim.y + threadIdx.y;
+  const int tid2  = blockIdx.z * blockDim.z + threadIdx.z;
+  
+  const int stride0 = blockDim.x * gridDim.x;
+  const int stride1 = blockDim.y * gridDim.y;
+  const int stride2 = blockDim.z * gridDim.z;
 
   const int Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
   const int Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
   const int Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
 
-  for (int which_gf = tid; which_gf < NUM_EVOL_GFS; which_gf++) {
-    size_t imin[3] = {(size_t) NGHOSTS, (size_t) NGHOSTS, (size_t) NGHOSTS};
-    size_t imax[3] = {
-      (size_t) (Nxx_plus_2NGHOSTS0 - NGHOSTS),
-      (size_t) (Nxx_plus_2NGHOSTS1 - NGHOSTS),
-      (size_t) (Nxx_plus_2NGHOSTS2 - NGHOSTS)
-    };
-    // After updating each face, adjust imin[] and imax[]
-    //   to reflect the newly-updated face extents.
-    FACE_UPDATE(which_gf, imin[0] - 1, imin[0], imin[1], imax[1], imin[2], imax[2], MINFACE, NUL, NUL);
-    imin[0]--;
-    FACE_UPDATE(which_gf, imax[0], imax[0] + 1, imin[1], imax[1], imin[2], imax[2], MAXFACE, NUL, NUL);
-    imax[0]++;
-
-    FACE_UPDATE(which_gf, imin[0], imax[0], imin[1] - 1, imin[1], imin[2], imax[2], NUL, MINFACE, NUL);
-    imin[1]--;
-    FACE_UPDATE(which_gf, imin[0], imax[0], imax[1], imax[1] + 1, imin[2], imax[2], NUL, MAXFACE, NUL);
-    imax[1]++;
-
-    FACE_UPDATE(which_gf, imin[0], imax[0], imin[1], imax[1], imin[2] - 1, imin[2], NUL, NUL, MINFACE);
-    imin[2]--;
-    FACE_UPDATE(which_gf, imin[0], imax[0], imin[1], imax[1], imax[2], imax[2] + 1, NUL, NUL, MAXFACE);
-    imax[2]++;
+  for (int i2 = tid2+imin2; i2 < imax2; i2+=stride2) {
+    for (int i1 = tid1+imin1; i1 < imax1; i1+=stride1) {
+      for (int i0 = tid0+imin0; i0 < imax0; i0+=stride0) {
+        gfs[IDX4(which_gf, i0, i1, i2)] = +3.0 * gfs[IDX4(which_gf, i0 + 1 * FACEX0, i1 + 1 * FACEX1, i2 + 1 * FACEX2)] -
+                                          3.0 * gfs[IDX4(which_gf, i0 + 2 * FACEX0, i1 + 2 * FACEX1, i2 + 2 * FACEX2)] +
+                                          1.0 * gfs[IDX4(which_gf, i0 + 3 * FACEX0, i1 + 3 * FACEX1, i2 + 3 * FACEX2)];
+      }
+    }
   }
 }
 
@@ -61,7 +58,52 @@ void apply_bcs_gpu(const params_struct *restrict params, REAL *restrict gfs) {
  */
 void apply_bcs(const commondata_struct *restrict commondata, const params_struct *restrict params, REAL *restrict gfs) {
   
+  // dim3 grid(GPU_NGRID0,GPU_NGRID1,GPU_NGRID2);
+  // dim3 block(GPU_NBLOCK0,GPU_NBLOCK1,GPU_NBLOCK2);
+  // Using multi-dimensional thread indexing doesn't
+  // work for some reason...
+  // dim3 block(1024,1,1);
   int grid = 1;
-  int block = NUM_EVOL_GFS;
-  apply_bcs_gpu<<<grid, block>>>(params, gfs);
+  int block = 1;
+
+  int Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2;
+  cudaMemcpy(&Nxx_plus_2NGHOSTS0, &params->Nxx_plus_2NGHOSTS0, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaCheckErrors(cudaMemcpy, "memory failed")
+  cudaMemcpy(&Nxx_plus_2NGHOSTS1, &params->Nxx_plus_2NGHOSTS1, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaCheckErrors(cudaMemcpy, "memory failed")
+  cudaMemcpy(&Nxx_plus_2NGHOSTS2, &params->Nxx_plus_2NGHOSTS2, sizeof(int), cudaMemcpyDeviceToHost);
+  cudaCheckErrors(cudaMemcpy, "memory failed")
+
+  for (int which_gf = 0; which_gf < NUM_EVOL_GFS; which_gf++) {
+    int imin[3] = {(int) NGHOSTS, (int) NGHOSTS, (int) NGHOSTS};
+    int imax[3] = {
+      (int) (Nxx_plus_2NGHOSTS0 - NGHOSTS),
+      (int) (Nxx_plus_2NGHOSTS1 - NGHOSTS),
+      (int) (Nxx_plus_2NGHOSTS2 - NGHOSTS)
+    };
+    for (int which_gz = 0; which_gz < NGHOSTS; which_gz++) {
+      // After updating each face, adjust imin[] and imax[]
+      //   to reflect the newly-updated face extents.
+      update_face<<<grid, block>>>(which_gf, params, gfs, imin[0] - 1, imin[0], imin[1], imax[1], imin[2], imax[2], MINFACE, NUL, NUL);
+      cudaCheckErrors(update_face, "kernel failed")
+      imin[0]--;
+      update_face<<<grid, block>>>(which_gf, params, gfs, imax[0], imax[0] + 1, imin[1], imax[1], imin[2], imax[2], MAXFACE, NUL, NUL);
+      cudaCheckErrors(update_face, "kernel failed")
+      imax[0]++;
+
+      update_face<<<grid, block>>>(which_gf, params, gfs, imin[0], imax[0], imin[1] - 1, imin[1], imin[2], imax[2], NUL, MINFACE, NUL);
+      cudaCheckErrors(update_face, "kernel failed")
+      imin[1]--;
+      update_face<<<grid, block>>>(which_gf, params, gfs, imin[0], imax[0], imax[1], imax[1] + 1, imin[2], imax[2], NUL, MAXFACE, NUL);
+      cudaCheckErrors(update_face, "kernel failed")
+      imax[1]++;
+
+      update_face<<<grid, block>>>(which_gf, params, gfs, imin[0], imax[0], imin[1], imax[1], imin[2] - 1, imin[2], NUL, NUL, MINFACE);
+      cudaCheckErrors(update_face, "kernel failed")
+      imin[2]--;
+      update_face<<<grid, block>>>(which_gf, params, gfs, imin[0], imax[0], imin[1], imax[1], imax[2], imax[2] + 1, NUL, NUL, MAXFACE);
+      cudaCheckErrors(update_face, "kernel failed")
+      imax[2]++;
+    }
+  }
 }
