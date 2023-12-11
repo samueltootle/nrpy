b1b1f795cbd8eca0cb221a1a52f1727408bec980
s.tootle <sdtootle@gmail.com>
2023-12-11 14:22:55 -0800

    Cleanup

--- project/wavetoy_gpu2/rhs_eval_gpu_warp_sm.cu
+++ project/wavetoy_gpu2/rhs_eval_gpu_warp_sm.cu
@@ -8,8 +8,6 @@ __global__ void compute_uu_dDDxx_gpu(const params_struct *restrict params,
                                  const REAL *restrict in_gfs,
                                  REAL *restrict aux_gfs) { 
 
-  // extern __shared__ REAL s_f[]; // 2-wide halo for 4th order FD
-
   const REAL & invdxx0 = d_params.invdxx0;
 
   const int & Nxx0 = d_params.Nxx0;
@@ -44,14 +42,6 @@ __global__ void compute_uu_dDDxx_gpu(const params_struct *restrict params,
   // Not sure how bad this is yet...
   // we do this to avoid shared memory
   bool active = (tid0 >= NGHOSTS && i < Nxx0 + NGHOSTS && lanex > 1 && lanex < 30);
-
-  // #ifdef DEBUG_IDX
-  // // if(!active && i < d_params.Nxx_plus_2NGHOSTS0)
-  //   if(warpx == 4 && blockIdx.y == 2)
-  // // if(blockIdx.y == 0 && blockIdx.x == 0 && k == 0)
-  //   printf("(%d, %d, %d)  - %d \t- %d \t- %d \t- %d \t- %d - %d\n", 
-  //     i, j, k, warpx, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, active);
-  // #endif
 
   if(active) {
     const REAL FDPart1tmp0 = -FDPart1_Rational_5_2 * uu;
@@ -109,18 +99,15 @@ __global__ void compute_uu_dDDyy_gpu(const params_struct *restrict params,
   uu_j0p1 = __shfl_down_sync(mask, uu, 1);
   uu_j0p2 = __shfl_down_sync(mask, uu, 2);
 
-  // Warp threads living in the ghost zones will be inactive
+  // Warp threads living in the ghost/halo zones will be inactive
   // Not sure how bad this is yet...
   // we do this to avoid shared memory
   bool active = (tid0 >= NGHOSTS && j < Nxx1 + NGHOSTS && lanex > 1 && lanex < 30);
+  
   #ifdef DEBUG_IDX
-  // if(!active && i < d_params.Nxx_plus_2NGHOSTS0)
-    if((lanex == 0 || lanex == 30) && (warpx == 3 || warpx == 4) && blockIdx.y == 2)
+  if((lanex == 0 || lanex == 30) && (warpx == 3 || warpx == 4) && blockIdx.y == 2)
     printf("(%d, %d, %d) \t- (%u, %u) \t- (%u, %u), (%u, %u) \t- %1.15e - %1.15e - %1.15e - %1.15e - %1.15e\n", 
       i, j, k, warpx, lanex, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, uu_j0m2, uu_j0m1, uu, uu_j0p2, uu_j0p2);
-  // if(blockIdx.y == 0 && blockIdx.x == 0 && k == 0)
-    // printf("(%d, %d, %d)  - %d \t- %d \t- %d \t- %d \t- %d - %d\n", 
-      // i, j, k, warpx, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, lane);
   #endif
   if(active) {
     const REAL FDPart1tmp0 = -FDPart1_Rational_5_2 * uu;
@@ -171,34 +158,20 @@ __global__ void compute_uu_dDDzz_gpu(const params_struct *restrict params,
   int i = tid1 + NGHOSTS;
   int j = tid2 + NGHOSTS;
   int k = tid0 - warpx * 2 * NGHOSTS;
-  // int tid = threadIdx.x + threadIdx.y * blockDim.x;;
   
   int globalIdx = IDX4(UUGF, i, j, k);
-  // if(lanex == 0 && blockIdx.y == 0)
-  // // if(blockIdx.y == 0 && blockIdx.x == 0 && k == 0)
-  //   printf("(%d, %d, %d) \t- %d \t- %d \t- %d \t- %d \t- %d\n", 
-  //     i, j, k, warpx, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y);
-  
 
-  
-  
   uu = in_gfs[globalIdx];
   uu_k0m2 = __shfl_up_sync(mask, uu, 2);
   uu_k0m1 = __shfl_up_sync(mask, uu, 1);
   uu_k0p1 = __shfl_down_sync(mask, uu, 1);
   uu_k0p2 = __shfl_down_sync(mask, uu, 2);
 
-  // Warp threads living in the ghost zones will be inactive
+  // Warp threads living in the ghost/halo zones will be inactive
   // Not sure how bad this is yet...
   // we do this to avoid shared memory
   bool active = (tid0 >= NGHOSTS && k < Nxx2 + NGHOSTS && lanex > 1 && lanex < 30);
-  //   #ifdef DEBUG_IDX
-  // // if(!active && i < d_params.Nxx_plus_2NGHOSTS0)
-  //   if(warpx == 1 && blockIdx.y == 2 && tid1 == 0)
-  // // if(blockIdx.y == 0 && blockIdx.x == 0 && k == 0)
-  //   printf("(%d, %d, %d)  - %d \t- %d \t- %d \t- %d \t- %d - %d\n", 
-  //     i, j, k, warpx, threadIdx.x, threadIdx.y, blockIdx.x, blockIdx.y, active);
-  // #endif
+
   if(active) {
     const REAL FDPart1tmp0 = -FDPart1_Rational_5_2 * uu;    
     
@@ -229,11 +202,12 @@ void compute_uu_dDDxx(const params_struct *restrict params,
   // Number of threads in the +/- halos. 32/warp, so 2 warps since it's 1D
   size_t halo_threads = 64;
   
-  // The between halo warps, each warp will need to overlap by NGHOST since
-  // we avoid using shared memory
+  // The between halo warps, each warp will have a halo of Nghosts that are inactive
+  // therefore we need to determine how many threads are needed to break up the interior
   size_t remaining_cells = Nxx_plus_2NGHOSTS0 - halo_threads;
   
   // Over estimate of number of threads needed to process data in the interior
+  // Note: 4.0 * NGHOSTS = cumulative halo per warp
   size_t interior_threads = (size_t) std::ceil((REAL)remaining_cells / (32.0 - 4.0 * NGHOSTS)) * 32u;
   
   size_t threads_in_x_dir = halo_threads + interior_threads;
@@ -260,8 +234,8 @@ void compute_uu_dDDyy(const params_struct *restrict params,
   size_t halo_warps = 2;
   size_t halo_threads = 2 * 32;
   
-  // The between halo warps, each warp will need to overlap by NGHOST since
-  // we avoid using shared memory
+  // The between halo warps, each warp will have a halo of Nghosts that are inactive
+  // therefore we need to determine how many threads are needed to break up the interior
   size_t remaining_cells = Nxx_plus_2NGHOSTS1 - halo_threads;
   
   // Over estimate of number of threads needed to process data in the interior
@@ -295,8 +269,8 @@ void compute_uu_dDDzz(const params_struct *restrict params,
   size_t halo_warps = 2;
   size_t halo_threads = 2 * 32;
   
-  // The between halo warps, each warp will need to overlap by NGHOST since
-  // we avoid using shared memory
+  // The between halo warps, each warp will have a halo of Nghosts that are inactive
+  // therefore we need to determine how many threads are needed to break up the interior
   size_t remaining_cells = Nxx_plus_2NGHOSTS2 - halo_threads;
   
   // Over estimate of number of threads needed to process data in the interior
@@ -362,20 +336,10 @@ void compute_rhs(const params_struct *restrict params,
                           const int Nxx0,
                           const int Nxx1,
                           const int Nxx2) {
-  // To ensure coalescence, we want retain reads in the x-direction
-  // i.e. the contiguous memory space, based on the standard
-  // instruction limits.  Each warp will attempt memory reads up
-  // to 128 bytes in a single instruction, in powers of 2,
-  // (e.g. 8,16,32,64,128), but this is dependent on the 
-  // compute capability of the GPU.  Here we dedicate
-  // one thread per data element we read in the x-direction.
+
+  // Maximize threads in x - fastest - direction
   size_t threads_in_x_dir = MIN(1024, Nxx0);
 
-  // Max threads in the y-direction.  Even if we can read
-  // the entire tile into shared memory, that doesn't mean
-  // we have enough threads per SM to process the entire tile.
-  // Therefore we can only have a maximum number of threads in the
-  // y direction and each thread will have to compute multiple points.
   size_t threads_in_y_dir = 1024 / threads_in_x_dir;
 
   size_t threads_in_z_dir = 1;
@@ -386,8 +350,6 @@ void compute_rhs(const params_struct *restrict params,
   // Setup our grid layout such that our tiles will iterate through the entire
   // numerical space
   dim3 grid_blocks(Nxx1 / threads_in_y_dir, Nxx2, 1);
-
-  // printf("SM_size : %lu , max: %lu\n\n\n", SM_size, maxMemPerBlock);
   
   compute_rhs_gpu<<<grid_blocks, block_threads>>>(params, in_gfs, aux_gfs, out_gfs);
   cudaCheckErrors(compute_rhs_gpu, "kernel failed")
