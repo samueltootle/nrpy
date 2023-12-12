0b749974b85f5576b2364afce21ad1dd9fa60114
s.tootle <sdtootle@gmail.com>
2023-12-12 15:36:29 -0800

    promote malloc/free to CUDA

--- project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_free__rfm__Spherical.cu
+++ project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_free__rfm__Spherical.cu
@@ -5,8 +5,8 @@
 void rfm_precompute_free__rfm__Spherical(const commondata_struct *restrict commondata, const params_struct *restrict params,
                                          rfm_struct *restrict rfmstruct) {
 #include "../set_CodeParameters.h"
-  free(rfmstruct->f0_of_xx0);
-  free(rfmstruct->f1_of_xx1);
-  free(rfmstruct->f1_of_xx1__D1);
-  free(rfmstruct->f1_of_xx1__DD11);
+  cudaFree(rfmstruct->f0_of_xx0);
+  cudaFree(rfmstruct->f1_of_xx1);
+  cudaFree(rfmstruct->f1_of_xx1__D1);
+  cudaFree(rfmstruct->f1_of_xx1__DD11);
 }
--- project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_malloc__rfm__Spherical.cu
+++ project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_malloc__rfm__Spherical.cu
@@ -5,16 +5,16 @@
  */
 void rfm_precompute_malloc__rfm__Spherical(const commondata_struct *restrict commondata, const params_struct *restrict params,
                                            rfm_struct *restrict rfmstruct) {
-  int Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, Nxx_plus_2NGHOSTS2;
-  cudaMemcpy(&Nxx_plus_2NGHOSTS0, &params->Nxx_plus_2NGHOSTS0, sizeof(int), cudaMemcpyDeviceToHost);
-  cudaCheckErrors(cudaMemcpy, "memory failed")
-  cudaMemcpy(&Nxx_plus_2NGHOSTS1, &params->Nxx_plus_2NGHOSTS1, sizeof(int), cudaMemcpyDeviceToHost);
-  cudaCheckErrors(cudaMemcpy, "memory failed")
-  cudaMemcpy(&Nxx_plus_2NGHOSTS2, &params->Nxx_plus_2NGHOSTS2, sizeof(int), cudaMemcpyDeviceToHost);
-  cudaCheckErrors(cudaMemcpy, "memory failed")
+  int const & Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
+  int const & Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
+  int const & Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
 
-  rfmstruct->f0_of_xx0 = (REAL *)malloc(sizeof(REAL) * Nxx_plus_2NGHOSTS0);
-  rfmstruct->f1_of_xx1 = (REAL *)malloc(sizeof(REAL) * Nxx_plus_2NGHOSTS1);
-  rfmstruct->f1_of_xx1__D1 = (REAL *)malloc(sizeof(REAL) * Nxx_plus_2NGHOSTS1);
-  rfmstruct->f1_of_xx1__DD11 = (REAL *)malloc(sizeof(REAL) * Nxx_plus_2NGHOSTS1);
+  cudaMalloc(&rfmstruct->f0_of_xx0, sizeof(sizeof(REAL) * Nxx_plus_2NGHOSTS0));
+  cudaCheckErrors(malloc, "Malloc failed")
+  cudaMalloc(&rfmstruct->f1_of_xx1, sizeof(sizeof(REAL) * Nxx_plus_2NGHOSTS1));
+  cudaCheckErrors(malloc, "Malloc failed")
+  cudaMalloc(&rfmstruct->f1_of_xx1__D1, sizeof(sizeof(REAL) * Nxx_plus_2NGHOSTS1));
+  cudaCheckErrors(malloc, "Malloc failed")
+  cudaMalloc(&rfmstruct->f1_of_xx1__DD11, sizeof(sizeof(REAL) * Nxx_plus_2NGHOSTS1));
+  cudaCheckErrors(malloc, "Malloc failed")
 }
