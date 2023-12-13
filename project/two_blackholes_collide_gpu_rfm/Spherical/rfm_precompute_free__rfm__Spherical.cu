2ccce0136267e1967ec5261c2ac02de8f11ca402
s.tootle <sdtootle@gmail.com>
2023-12-12 16:12:12 -0800

    Remove unnecessary include

--- project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_free__rfm__Spherical.cu
+++ project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_free__rfm__Spherical.cu
@@ -4,7 +4,6 @@
  */
 void rfm_precompute_free__rfm__Spherical(const commondata_struct *restrict commondata, const params_struct *restrict params,
                                          rfm_struct *restrict rfmstruct) {
-#include "../set_CodeParameters.h"
   cudaFree(rfmstruct->f0_of_xx0);
   cudaFree(rfmstruct->f1_of_xx1);
   cudaFree(rfmstruct->f1_of_xx1__D1);
