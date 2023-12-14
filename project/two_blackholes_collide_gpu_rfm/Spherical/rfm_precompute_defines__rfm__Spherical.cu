e97b392ad08c5b049a29cd884234c31fbe22678f
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-12-13 16:55:01 -0800

    Resolved pointer issues since rfm struct doesn't reside on the device

--- project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_defines__rfm__Spherical.cu
+++ project/two_blackholes_collide_gpu_rfm/Spherical/rfm_precompute_defines__rfm__Spherical.cu
@@ -5,33 +5,41 @@
  */
 
 __global__
-void rfm_precompute_defines__rfm__Spherical_xx0_gpu(rfm_struct *restrict rfmstruct, const REAL *restrict xx0) {
-  for (int i0 = 0; i0 < d_params.Nxx_plus_2NGHOSTS0; i0++) {
+void rfm_precompute_defines__rfm__Spherical_xx0_gpu(REAL *restrict f0_of_xx0, const REAL *restrict xx0) {
+  const int tid0 = threadIdx.x + blockIdx.x*blockDim.x;
+  const int stride0 = blockDim.x * gridDim.x;
+  for (int i0 = tid0; i0 < d_params.Nxx_plus_2NGHOSTS0; i0+=stride0) {
     const REAL xx = xx0[i0];
-    rfmstruct->f0_of_xx0[i0] = xx;
+    f0_of_xx0[i0] = xx;
   }
 }
 
 __global__
-void rfm_precompute_defines__rfm__Spherical_xx1_gpu(rfm_struct *restrict rfmstruct, const REAL *restrict xx1) {
-  for (int i1 = 0; i1 < d_params.Nxx_plus_2NGHOSTS1; i1++) {
+void rfm_precompute_defines__rfm__Spherical_xx1_gpu(REAL *restrict f1_of_xx1, const REAL *restrict xx1) {
+  const int tid0 = threadIdx.x + blockIdx.x*blockDim.x;
+  const int stride0 = blockDim.x * gridDim.x;
+  for (int i1 = tid0; i1 < d_params.Nxx_plus_2NGHOSTS1; i1+=stride0) {
     const REAL xx = xx1[i1];
-    rfmstruct->f1_of_xx1[i1] = sin(xx);
+    f1_of_xx1[i1] = sin(xx);
   }
 }
 
 __global__
-void rfm_precompute_defines__rfm__Spherical_xx1__D1_gpu(rfm_struct *restrict rfmstruct, const REAL *restrict xx1) {
-  for (int i1 = 0; i1 < d_params.Nxx_plus_2NGHOSTS1; i1++) {
+void rfm_precompute_defines__rfm__Spherical_xx1__D1_gpu(REAL *restrict f1_of_xx1__D1, const REAL *restrict xx1) {
+  const int tid0 = threadIdx.x + blockIdx.x*blockDim.x;
+  const int stride0 = blockDim.x * gridDim.x;
+  for (int i1 = tid0; i1 < d_params.Nxx_plus_2NGHOSTS1; i1+=stride0) {
     const REAL xx = xx1[i1];
-    rfmstruct->f1_of_xx1__D1[i1] = cos(xx);
+    f1_of_xx1__D1[i1] = cos(xx);
   }
 }
 
 __global__
-void rfm_precompute_defines__rfm__Spherical_xx1__DD11_gpu(rfm_struct *restrict rfmstruct) {
-  for (int i1 = 0; i1 < d_params.Nxx_plus_2NGHOSTS1; i1++) {
-    rfmstruct->f1_of_xx1__DD11[i1] = -rfmstruct->f1_of_xx1[i1];
+void rfm_precompute_defines__rfm__Spherical_xx1__DD11_gpu(REAL *restrict f1_of_xx1__DD11, REAL *restrict f1_of_xx1) {
+  const int tid0 = threadIdx.x + blockIdx.x*blockDim.x;
+  const int stride0 = blockDim.x * gridDim.x;
+  for (int i1 = tid0; i1 < d_params.Nxx_plus_2NGHOSTS1; i1+=stride0) {
+    f1_of_xx1__DD11[i1] = -f1_of_xx1[i1];
   }
 }
 
@@ -49,14 +57,14 @@ void rfm_precompute_defines__rfm__Spherical(const commondata_struct *restrict co
     grid_blocks = dim3((Nx + tx - 1)/tx, 1, 1);
   };
   set_grid_block(Nxx_plus_2NGHOSTS0);
-  rfm_precompute_defines__rfm__Spherical_xx0_gpu<<<grid_blocks, block_threads, 0, stream1>>>(rfmstruct, xx[0]);
+  rfm_precompute_defines__rfm__Spherical_xx0_gpu<<<grid_blocks, block_threads, 0, stream1>>>(rfmstruct->f0_of_xx0, xx[0]);
   cudaCheckErrors(rfm_precompute_defines__rfm__Spherical_xx0_gpu, "kernel failed");
 
   set_grid_block(Nxx_plus_2NGHOSTS1);
-  rfm_precompute_defines__rfm__Spherical_xx1_gpu<<<grid_blocks, block_threads, 0, stream2>>>(rfmstruct, xx[1]);
+  rfm_precompute_defines__rfm__Spherical_xx1_gpu<<<grid_blocks, block_threads, 0, stream2>>>(rfmstruct->f1_of_xx1, xx[1]);
   cudaCheckErrors(rfm_precompute_defines__rfm__Spherical_xx1_gpu, "kernel failed");
-  rfm_precompute_defines__rfm__Spherical_xx1__D1_gpu<<<grid_blocks, block_threads, 0, stream3>>>(rfmstruct, xx[1]);
+  rfm_precompute_defines__rfm__Spherical_xx1__D1_gpu<<<grid_blocks, block_threads, 0, stream3>>>(rfmstruct->f1_of_xx1__D1, xx[1]);
   cudaCheckErrors(rfm_precompute_defines__rfm__Spherical_xx1__D1_gpu, "kernel failed");
-  rfm_precompute_defines__rfm__Spherical_xx1__DD11_gpu<<<grid_blocks, block_threads, 0, stream2>>>(rfmstruct);
+  rfm_precompute_defines__rfm__Spherical_xx1__DD11_gpu<<<grid_blocks, block_threads, 0, stream2>>>(rfmstruct->f1_of_xx1__DD11, rfmstruct->f1_of_xx1);
   cudaCheckErrors(rfm_precompute_defines__rfm__Spherical_xx1__DD11_gpu, "kernel failed");
 }
