9ce34fa486ff28b45a746e7a55eeec85aaba528d
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-12-19 20:15:05 -0800

    Start adding MoL

--- project/two_blackholes_collide_gpu_rfm/main.cu
+++ project/two_blackholes_collide_gpu_rfm/main.cu
@@ -66,15 +66,15 @@ int main(int argc, const char *argv[]) {
 
   // // Step 5: MAIN SIMULATION LOOP
   // while (commondata.time < commondata.t_final) { // Main loop to progress forward in time.
-  //   // Step 5.a: Main loop, part 1: Output diagnostics
+    // Step 5.a: Main loop, part 1: Output diagnostics
     diagnostics(&commondata, griddata);
 
-  //   // Step 5.b: Main loop, part 2 (pre_MoL_step_forward_in_time): Prepare to step forward in time
-  //   // (nothing here; specify by setting pre_MoL_step_forward_in_time string in register_CFunction_main_c().)
+    // Step 5.b: Main loop, part 2 (pre_MoL_step_forward_in_time): Prepare to step forward in time
+    // (nothing here; specify by setting pre_MoL_step_forward_in_time string in register_CFunction_main_c().)
 
-  //   // Step 5.c: Main loop, part 3: Step forward in time using Method of Lines with RK4 algorithm,
-  //   //           applying outgoing radiation boundary conditions.
-  //   MoL_step_forward_in_time(&commondata, griddata);
+    // Step 5.c: Main loop, part 3: Step forward in time using Method of Lines with RK4 algorithm,
+    //           applying outgoing radiation boundary conditions.
+    MoL_step_forward_in_time(&commondata, griddata);
 
   //   // Step 5.d: Main loop, part 4 (post_MoL_step_forward_in_time): Finish up step in time
   //   // (nothing here; specify by setting post_MoL_step_forward_in_time string in register_CFunction_main_c().)
