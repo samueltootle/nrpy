c55a1f9c40f943250bb4a648b8c8b3cf0394ef49
Samuel Tootle <tootle@itp.uni-frankfurt.de>
2023-12-22 14:17:37 -0800

    GPU version

--- project/two_blackholes_collide_gpu_rfm/apply_bcs_outerextrap_and_inner.cu
+++ project/two_blackholes_collide_gpu_rfm/apply_bcs_outerextrap_and_inner.cu
@@ -1,5 +1,58 @@
 #include "BHaH_defines.h"
 #include "BHaH_function_prototypes.h"
+#include "BHaH_gpu_defines.h"
+#include "BHaH_gpu_function_prototypes.h"
+
+__global__
+void apply_bcs_outerextrap_and_inner_only_gpu(const int num_pure_outer_boundary_points, const int which_gz, const int dirn,
+  const outerpt_bc_struct *restrict pure_outer_bc_array, REAL *restrict gfs){
+  int const & Nxx_plus_2NGHOSTS0 = d_params.Nxx_plus_2NGHOSTS0;
+  int const & Nxx_plus_2NGHOSTS1 = d_params.Nxx_plus_2NGHOSTS1;
+  int const & Nxx_plus_2NGHOSTS2 = d_params.Nxx_plus_2NGHOSTS2;
+  
+  // Thread indices
+  // Global data index - expecting a 1D dataset
+    const int tid0 = threadIdx.x + blockIdx.x*blockDim.x;
+    // Thread strides
+    const int stride0 = blockDim.x * gridDim.x;
+
+  for (int idx2d = tid0; idx2d < num_pure_outer_boundary_points; idx2d+=stride0) {
+    const short i0 = pure_outer_bc_array[idx2d].i0;
+    const short i1 = pure_outer_bc_array[idx2d].i1;
+    const short i2 = pure_outer_bc_array[idx2d].i2;
+    const short FACEX0 = pure_outer_bc_array[idx2d].FACEX0;
+    const short FACEX1 = pure_outer_bc_array[idx2d].FACEX1;
+    const short FACEX2 = pure_outer_bc_array[idx2d].FACEX2;
+    const int idx_offset0 = IDX3(i0, i1, i2);
+    const int idx_offset1 = IDX3(i0 + 1 * FACEX0, i1 + 1 * FACEX1, i2 + 1 * FACEX2);
+    const int idx_offset2 = IDX3(i0 + 2 * FACEX0, i1 + 2 * FACEX1, i2 + 2 * FACEX2);
+    const int idx_offset3 = IDX3(i0 + 3 * FACEX0, i1 + 3 * FACEX1, i2 + 3 * FACEX2);
+    for (int which_gf = 0; which_gf < NUM_EVOL_GFS; which_gf++) {
+      // *** Apply 2nd-order polynomial extrapolation BCs to all outer boundary points. ***
+      gfs[IDX4pt(which_gf, idx_offset0)] =
+          + 3.0 * gfs[IDX4pt(which_gf, idx_offset1)] 
+          - 3.0 * gfs[IDX4pt(which_gf, idx_offset2)] 
+          + 1.0 * gfs[IDX4pt(which_gf, idx_offset3)];
+    }
+  }
+}
+
+void apply_bcs_outerextrap_and_inner_only(const bc_struct *restrict bcstruct, REAL *restrict gfs) {
+  const bc_info_struct *bc_info = &bcstruct->bc_info;
+  for (int which_gz = 0; which_gz < NGHOSTS; which_gz++) {
+    for (int dirn = 0; dirn < 3; dirn++) {
+      if (bc_info->num_pure_outer_boundary_points[which_gz][dirn] > 0) {
+        int num_pure = bc_info->num_pure_outer_boundary_points[which_gz][dirn];
+        size_t block_threadsx = MIN(1024,num_pure);
+        size_t grid_blocks = (num_pure + block_threadsx -1) / block_threadsx;
+        size_t gz_idx = dirn + (3 * which_gz);
+        apply_bcs_outerextrap_and_inner_only_gpu<<<grid_blocks, block_threadsx>>>(
+          num_pure, which_gz, dirn, bcstruct->pure_outer_bc_array[gz_idx], gfs);
+      }
+    }
+  }
+}
+
 /*
  * #Suppose the outer boundary point is at the i0=max(i0) face. Then we fit known data at i0-3, i0-2, and i0-1
  * #  to the unique quadratic polynomial that passes through those points, and fill the data at
@@ -13,10 +66,6 @@
  */
 void apply_bcs_outerextrap_and_inner(const commondata_struct *restrict commondata, const params_struct *restrict params,
                                      const bc_struct *restrict bcstruct, REAL *restrict gfs) {
-#include "set_CodeParameters.h"
-
-  // Unpack bc_info from bcstruct
-  const bc_info_struct *bc_info = &bcstruct->bc_info;
 
   ////////////////////////////////////////////////////////
   // STEP 1 of 2: Apply BCs to pure outer boundary points.
@@ -29,39 +78,7 @@ void apply_bcs_outerextrap_and_inner(const commondata_struct *restrict commondat
   //              then +/- x1 faces, finally +/- x2 faces,
   //              filling in the edges as we go.
   // Spawn N OpenMP threads, either across all cores, or according to e.g., taskset.
-#pragma omp parallel
-  {
-    for (int which_gz = 0; which_gz < NGHOSTS; which_gz++)
-      for (int dirn = 0; dirn < 3; dirn++) {
-        // This option results in about 1.6% slower runtime for SW curvilinear at 64x24x24 on 8-core Ryzen 9 4900HS
-        //#pragma omp for collapse(2)
-        // for(int which_gf=0;which_gf<NUM_EVOL_GFS;which_gf++) for(int idx2d=0;idx2d<bc_info->num_pure_outer_boundary_points[which_gz][dirn];idx2d++)
-        // {
-        //  {
-        // Don't spawn a thread if there are no boundary points to fill; results in a nice little speedup.
-        if (bc_info->num_pure_outer_boundary_points[which_gz][dirn] > 0) {
-#pragma omp for // threads have been spawned; here we distribute across them
-          for (int idx2d = 0; idx2d < bc_info->num_pure_outer_boundary_points[which_gz][dirn]; idx2d++) {
-            const short i0 = bcstruct->pure_outer_bc_array[dirn + (3 * which_gz)][idx2d].i0;
-            const short i1 = bcstruct->pure_outer_bc_array[dirn + (3 * which_gz)][idx2d].i1;
-            const short i2 = bcstruct->pure_outer_bc_array[dirn + (3 * which_gz)][idx2d].i2;
-            const short FACEX0 = bcstruct->pure_outer_bc_array[dirn + (3 * which_gz)][idx2d].FACEX0;
-            const short FACEX1 = bcstruct->pure_outer_bc_array[dirn + (3 * which_gz)][idx2d].FACEX1;
-            const short FACEX2 = bcstruct->pure_outer_bc_array[dirn + (3 * which_gz)][idx2d].FACEX2;
-            const int idx_offset0 = IDX3(i0, i1, i2);
-            const int idx_offset1 = IDX3(i0 + 1 * FACEX0, i1 + 1 * FACEX1, i2 + 1 * FACEX2);
-            const int idx_offset2 = IDX3(i0 + 2 * FACEX0, i1 + 2 * FACEX1, i2 + 2 * FACEX2);
-            const int idx_offset3 = IDX3(i0 + 3 * FACEX0, i1 + 3 * FACEX1, i2 + 3 * FACEX2);
-            for (int which_gf = 0; which_gf < NUM_EVOL_GFS; which_gf++) {
-              // *** Apply 2nd-order polynomial extrapolation BCs to all outer boundary points. ***
-              gfs[IDX4pt(which_gf, idx_offset0)] =
-                  +3.0 * gfs[IDX4pt(which_gf, idx_offset1)] - 3.0 * gfs[IDX4pt(which_gf, idx_offset2)] + 1.0 * gfs[IDX4pt(which_gf, idx_offset3)];
-            }
-          }
-        }
-      }
-  }
-
+  apply_bcs_outerextrap_and_inner_only(bcstruct, gfs);
   ///////////////////////////////////////////////////////
   // STEP 2 of 2: Apply BCs to inner boundary points.
   //              These map to either the grid interior
