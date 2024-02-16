7644aeed1bb993a356d9dd7d9a6ef187656438ca
s.tootle <sdtootle@gmail.com>
2024-02-16 13:11:36 -0800

    add params dump to header

--- project/two_blackholes_collide/trusted_data_dump/trusted_data_dump_prototypes.h
+++ project/two_blackholes_collide/trusted_data_dump/trusted_data_dump_prototypes.h
@@ -1,1 +1,2 @@
-void dump_common_data(commondata_struct *restrict commondata, const char* fname);
+void dump_common_data(commondata_struct *restrict commondata, const char* suffix);
+void dump_param_struct(const int grid, params_struct *restrict commondata, const char* suffix);
