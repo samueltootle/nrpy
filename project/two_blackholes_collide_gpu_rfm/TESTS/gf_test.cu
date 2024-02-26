8b33246b1ba7076d10dd55f8db3e172361133e67
s.tootle <sdtootle@gmail.com>
2024-02-26 11:57:46 -0800

    disabled GF dump for now

--- project/two_blackholes_collide_gpu_rfm/TESTS/gf_test.cu
+++ project/two_blackholes_collide_gpu_rfm/TESTS/gf_test.cu
@@ -4,24 +4,24 @@
 #include "TEST_prototypes.h"
 // PREFIX: e.g. auxvars, evolvars
 // SUFFIX: e.g. post_initial, post_rk1
-void dump_gf_array(const int grid,const params_struct *restrict params, const REAL *restrict gfs, 
-  const char* prefix, const char* suffix, const int numgfs) {
+// void dump_gf_array(const int grid,const params_struct *restrict params, const REAL *restrict gfs, 
+//   const char* prefix, const char* suffix, const int numgfs) {
   
-  char fname[100];
-  sprintf(fname, "TEST/dumps/gfs_%s_grid_%d_%s.txt", prefix, grid, suffix);
-  // printf("TEST: %s\n", fname);
-  FILE* fp = fopen(fname, "w");
+//   char fname[100];
+//   sprintf(fname, "TEST/dumps/gfs_%s_grid_%d_%s.txt", prefix, grid, suffix);
+//   // printf("TEST: %s\n", fname);
+//   FILE* fp = fopen(fname, "w");
   
-  const int Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
-  const int Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
-  const int Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
-  const int N = Nxx_plus_2NGHOSTS0 * Nxx_plus_2NGHOSTS1 * Nxx_plus_2NGHOSTS2;
-  for(int gf = 0; gf < numgfs; ++gf) {
-    for(int i = 0; i < N; ++i) {
-      int idx = IDX4pt(gf, i);
-      fprintf(fp, "%+1.15f\n", gfs[idx]);
-    }
-  }
-  fclose(fp);
-}
+//   const int Nxx_plus_2NGHOSTS0 = params->Nxx_plus_2NGHOSTS0;
+//   const int Nxx_plus_2NGHOSTS1 = params->Nxx_plus_2NGHOSTS1;
+//   const int Nxx_plus_2NGHOSTS2 = params->Nxx_plus_2NGHOSTS2;
+//   const int N = Nxx_plus_2NGHOSTS0 * Nxx_plus_2NGHOSTS1 * Nxx_plus_2NGHOSTS2;
+//   for(int gf = 0; gf < numgfs; ++gf) {
+//     for(int i = 0; i < N; ++i) {
+//       int idx = IDX4pt(gf, i);
+//       fprintf(fp, "%+1.15f\n", gfs[idx]);
+//     }
+//   }
+//   fclose(fp);
+// }
 #endif
