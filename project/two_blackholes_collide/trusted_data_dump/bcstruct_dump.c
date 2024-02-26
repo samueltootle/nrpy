93b4d01fdf00fb424098959c3f1d95b9bae87f79
s.tootle <sdtootle@gmail.com>
2024-02-26 12:11:55 -0800

    Draft to dump bcstruct

---
+++ project/two_blackholes_collide/trusted_data_dump/bcstruct_dump.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include "../BHaH_defines.h"
+#ifdef GPU_TESTS
+#include "trusted_data_dump_prototypes.h"
+void dump_bcstruct(const bc_struct *restrict bcstruct, const char* suffix) {
+  char fname[100];
+  sprintf(fname, "trusted_data_dump/dumps/bcstruct_%s.txt", suffix);
+  // printf("TEST bcstruct: %s\n", fname);
+  FILE* fp = fopen(fname, "w");
+  fprintf(fp, "%+d\n", bcstruct->bc_info.num_inner_boundary_points);
+  for(int i = 0; i < NGHOSTS; ++i) {
+    for(int j = 0; j < 3; ++j) {
+      fprintf(fp, "%+d\n", bcstruct->bc_info.num_pure_outer_boundary_points[i][j]);
+    }
+  }
+  for(int i = 0; i < NGHOSTS; ++i) {
+    for(int j = 0; j < 6; ++j) {
+      for(int k = 0; k < 6; ++k) {
+        fprintf(fp, "%+d\n", bcstruct->bc_info.bc_loop_bounds[i][j][k]);
+      }
+    }
+  }
+  
+  for(int i = 0; i < bcstruct->bc_info.num_inner_boundary_points; ++i) {
+    fprintf(fp, "%+d\n", bcstruct->inner_bc_array[i].dstpt);
+    fprintf(fp, "%+d\n", bcstruct->inner_bc_array[i].srcpt);
+    for(int j = 0; j < 10; ++j) {
+      fprintf(fp, "%+d\n", bcstruct->inner_bc_array[i].parity[j]);
+    }
+  }
+
+  // for(int i = 0; i < NGHOSTS *3; ++i) {
+
+  // }
+  // fprintf(fp, "%+1.15f\n", commondata->BH1_mass);
+
+  // fprintf(fp, "%+d\n", commondata->NUMGRIDS);
+  fclose(fp);
+}
+#endif
--- project/two_blackholes_collide/trusted_data_dump/trusted_data_dump_prototypes.h
+++ project/two_blackholes_collide/trusted_data_dump/trusted_data_dump_prototypes.h
@@ -4,4 +4,5 @@ void dump_param_struct(const int grid, const params_struct *restrict commondata,
 void dump_gf_array(const int grid, const params_struct *restrict params, const REAL *restrict gfs, 
   const char* prefix, const char* suffix, const int numgfs);
 void dump_coord_direction(const int grid, const REAL *restrict xx, const char* dir, const int Nxx);
+void dump_bcstruct(const bc_struct *restrict bcstruct, const char* suffix);
 #endif
