1274be958f1dea5ae5bd4bcbad6670ed8fca2e92
Samuel Tootle <sdtootle@gmail.com>
2024-04-09 09:39:39 -0700

    Resolve gridstruct issues for multiple grids

--- project/two_blackholes_collide_gpu_rfm/CoordSystem_hash_setup.cpp
+++ project/two_blackholes_collide_gpu_rfm/CoordSystem_hash_setup.cpp
@@ -5,5 +5,7 @@
  */
 void CoordSystem_hash_setup(commondata_struct *restrict commondata, griddata_struct *restrict griddata) {
   // Set CoordSystem_hash, used for multi-coordinate-system evolutions. Hash is #define'd in BHaH_defines.h
-  griddata[0].params.CoordSystem_hash = SPHERICAL;
+  for(int grid = 0; grid < commondata->NUMGRIDS; ++grid) {
+    griddata[grid].params.CoordSystem_hash = SPHERICAL;
+  }
 }
