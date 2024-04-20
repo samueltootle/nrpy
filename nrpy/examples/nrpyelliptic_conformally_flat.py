7cf595b39fa865024fd212743231cebd28ece516
s.tootle <sdtootle@gmail.com>
2024-04-20 13:27:12 -0700

    Revert to cuda

--- nrpy/examples/nrpyelliptic_conformally_flat.py
+++ nrpy/examples/nrpyelliptic_conformally_flat.py
@@ -376,9 +376,9 @@ Makefile.output_CFunctions_function_prototypes_and_construct_Makefile(
     project_dir=project_dir,
     project_name=project_name,
     exec_or_library_name=project_name,
-    # CC="nvcc",
-    # code_ext="cu",
-    # compiler_opt_option="nvcc",
+    CC="nvcc",
+    code_ext="cu",
+    compiler_opt_option="nvcc",
 )
 print(
     f"Finished! Now go into project/{project_name} and type `make` to build, then ./{project_name} to run."
