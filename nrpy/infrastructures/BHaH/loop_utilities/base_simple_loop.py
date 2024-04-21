475d31a260cbfbe48ff34b74d366c68bb5d44df8
Samuel Tootle <sdtootle@gmail.com>
2024-04-20 23:54:49 -0700

    Update vars based on GPU setup

--- nrpy/infrastructures/BHaH/loop_utilities/base_simple_loop.py
+++ nrpy/infrastructures/BHaH/loop_utilities/base_simple_loop.py
@@ -94,9 +94,9 @@ class base_simple_loop:
         # 'Read_xxs': read the xx[3][:] 1D coordinate arrays, as some interior dependency exists
         if self.read_xxs:
             self.read_rfm_xx_arrays = [
-                "const REAL xx0 = xx[0][i0];",
-                "const REAL xx1 = xx[1][i1];",
-                "const REAL xx2 = xx[2][i2];",
+                "const REAL xx0 = x0[i0];",
+                "const REAL xx1 = x1[i1];",
+                "const REAL xx2 = x2[i2];",
             ]
 
         # 'enable_rfm_precompute': enable pre-computation of reference metric
