c469a5780800db06551f01945168a0687a8f2242
Samuel Tootle <sdtootle@gmail.com>
2024-04-22 12:53:22 -0700

    Update to cuda register rfm. wip

--- nrpy/infrastructures/BHaH/grid_management/cuda/register_rfm_precompute.py
+++ nrpy/infrastructures/BHaH/grid_management/cuda/register_rfm_precompute.py
@@ -9,9 +9,8 @@ Authors: Samuel D. Tootle
 
 from typing import List
 
-import nrpy.c_function as cfc
-from nrpy.helpers.generic import superfast_uniq
-from nrpy.infrastructures.BHaH.BHaH_defines_h import register_BHaH_defines
+import nrpy.helpers.gpu_kernel as gputils
+from nrpy.helpers.expr_tree import get_unique_expression_symbols
 from nrpy.infrastructures.BHaH.grid_management.base_register_rfm_precompute import base_register_CFunctions_rfm_precompute
 from nrpy.infrastructures.BHaH.grid_management.cuda.rfm_precompute import ReferenceMetricPrecompute
 # import nrpy.helpers.gpu_kernel as gputils
@@ -44,11 +43,60 @@ class register_CFunctions_rfm_precompute(base_register_CFunctions_rfm_precompute
                 cfunc_type = "void"
                 name = "rfm_precompute_" + func[0]
                 params = "const commondata_struct *restrict commondata, const params_struct *restrict params, rfm_struct *restrict rfmstruct"
-                if func[0] == "defines":
-                    params += ", REAL *restrict xx[3]"
 
                 body = " "
                 body += func[1]
+                
+                self.function_dict[name] = {
+                    'desc' : desc,
+                    'cfunc_type' : cfunc_type,
+                    'params' : params,
+                    'body' : body,
+                    'CoordSystem' : CoordSystem,
+                }
+            
+            for func, kernel_dicts in [
+                ("defines", rfm_precompute.rfm_struct__define_kernel_dict),
+            ]:
+
+                desc = f"rfm_precompute_{func}: reference metric precomputed lookup arrays: {func[0]}"
+                cfunc_type = "void"
+                name = "rfm_precompute_" + func
+                params = "const commondata_struct *restrict commondata, const params_struct *restrict params, rfm_struct *restrict rfmstruct"
+                params += ", REAL *restrict xx[3]"
+
+                body = " "
+                for i in range(3):
+                    body += f"const REAL *restrict x{i} = xx[{i}];\n"
+                
+                for key_sym, kernel_dict in kernel_dicts.items():
+                    # These should all be in paramstruct?
+                    unique_symbols = get_unique_expression_symbols(kernel_dict['expr'], exclude=[f'xx{i}' for i in range(3)])
+                    kernel_body = ""
+                    for sym in unique_symbols:
+                        kernel_body += f"const REAL {sym} = d_params->{sym};\n" 
+                    kernel_body += kernel_dict['body']
+                    device_kernel = gputils.GPU_Kernel(
+                        kernel_body,
+                        {
+                            f'{key_sym}' : 'REAL *restrict',
+                            f'{kernel_dict['coord']}' : 'const REAL *restrict'
+                        }
+                        f"{name}__{key}_gpu",
+                        launch_dict= {
+                            'blocks_per_grid' : [],
+                            'threads_per_block' : ["32", "NGHOSTS"],
+                            'stream' : "default"
+                        },
+                        fp_type=self.fp_type,
+                        comments=f"GPU Kernel to precompute metric quantity {key}.",
+                    )
+                    body += "{\n"
+                    body += device_kernel.launch_block()
+                    body += device_kernel.c_function_call()
+                    body += "}\n"
+                    
+                # body += func[1]
                 
                 self.function_dict[name] = {
                     'desc' : desc,
