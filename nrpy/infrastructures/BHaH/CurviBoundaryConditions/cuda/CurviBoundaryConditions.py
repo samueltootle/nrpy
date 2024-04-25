726fc8593cbe5cc567067a0135c828023aafe3a6
Samuel Tootle <sdtootle@gmail.com>
2024-04-25 14:12:38 -0700

    Manually set fp_type to double for curvi BCs to avoid issues

--- nrpy/infrastructures/BHaH/CurviBoundaryConditions/cuda/CurviBoundaryConditions.py
+++ nrpy/infrastructures/BHaH/CurviBoundaryConditions/cuda/CurviBoundaryConditions.py
@@ -910,6 +910,8 @@ class CurviBoundaryConditions_register_C_functions(
         set_parity_on_auxevol: bool = False,
         fp_type: str = "double",
     ) -> None:
+        # Without doing double precision when setting CurviBCs, many of the bcstruct setup will error out
+        fp_type = "double"
         super().__init__(
             list_of_CoordSystems,
             radiation_BC_fd_order=radiation_BC_fd_order,
