fc3b1a945f6842e4cef8009e61d80c773d46339e
s.tootle <sdtootle@gmail.com>
2025-02-27 09:58:12 -0800

    Refactor macros to use X padding

--- nrpy/infrastructures/BHaH/BHaH_defines_h.py
+++ nrpy/infrastructures/BHaH/BHaH_defines_h.py
@@ -348,23 +348,23 @@ def output_BHaH_defines_h(
 // IDX4: Converts 4D grid indices (gf, i, j, k) into a 1D array index using the strides
 //       Nxx_plus_2NGHOSTS0, Nxx_plus_2NGHOSTS1, and Nxx_plus_2NGHOSTS2. This macro assumes
 //       that the "i" index varies fastest in memory.
-#define IDX4(gf, i, j, k) ((i) + Nxx_plus_2NGHOSTS0 * ((j) + Nxx_plus_2NGHOSTS1 * ((k) + Nxx_plus_2NGHOSTS2 * (gf))))
+#define IDX4(gf, i, j, k) ((i) + Nxx_plus_2NGHOSTS0_PADDED * ((j) + Nxx_plus_2NGHOSTS1 * ((k) + Nxx_plus_2NGHOSTS2 * (gf))))
 // IDX4P: Similar to IDX4, but retrieves grid dimensions from the provided parameter structure
 //        "params" instead of using global variables.
 #define IDX4P(params, gf, i, j, k)                                                                                                                   \
   ((i) + (params)->Nxx_plus_2NGHOSTS0 * ((j) + (params)->Nxx_plus_2NGHOSTS1 * ((k) + (params)->Nxx_plus_2NGHOSTS2 * (gf))))
 // IDX4pt: Computes the 1D index offset for a given grid function index (gf) based on an existing index (idx)
 //         by using the total number of elements in one grid function, defined as the product of the grid strides.
-#define IDX4pt(gf, idx) ((idx) + (Nxx_plus_2NGHOSTS0 * Nxx_plus_2NGHOSTS1 * Nxx_plus_2NGHOSTS2) * (gf))
+#define IDX4pt(gf, idx) ((idx) + (Nxx_plus_2NGHOSTS0_PADDED * Nxx_plus_2NGHOSTS1 * Nxx_plus_2NGHOSTS2) * (gf))
 // IDX4ptP: Similar to IDX4pt, but retrieves grid dimensions from the provided parameter structure
 //        "params" instead of using global variables.
 #define IDX4Ppt(params, gf, idx) \
   ((idx) + ((params)->Nxx_plus_2NGHOSTS0 * (params)->Nxx_plus_2NGHOSTS1 * (params)->Nxx_plus_2NGHOSTS2) * (gf))
 // IDX3: Converts 3D grid indices (i, j, k) into a 1D array index using the strides Nxx_plus_2NGHOSTS0
 //       and Nxx_plus_2NGHOSTS1. Like IDX4, this macro assumes the "i" index varies fastest.
-#define IDX3(i, j, k) ((i) + Nxx_plus_2NGHOSTS0 * ((j) + Nxx_plus_2NGHOSTS1 * ((k))))
+#define IDX3(i, j, k) ((i) + Nxx_plus_2NGHOSTS0_PADDED * ((j) + Nxx_plus_2NGHOSTS1 * ((k))))
 // IDX3P: Similar to IDX3, but retrieves grid dimensions from the provided parameter structure "params".
-#define IDX3P(params, i, j, k) ((i) + (params)->Nxx_plus_2NGHOSTS0 * ((j) + (params)->Nxx_plus_2NGHOSTS1 * ((k))))
+#define IDX3P(params, i, j, k) ((i) + (params)->Nxx_plus_2NGHOSTS0_PADDED * ((j) + (params)->Nxx_plus_2NGHOSTS1 * ((k))))
 // END: Indexing macros
 
 // ----------------------------
